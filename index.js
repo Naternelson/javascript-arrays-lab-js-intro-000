bdd4eb8b91ff287d816e471538a5f88502195a78
Nathan <7nater.nelson@gmail.com>
2020-01-10 19:09:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyRemoveLastKitten() {
   return kittens
 }
 
-fucntion destructivelyPrependKitten(name) {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens
 }
