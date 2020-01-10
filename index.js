970b31e4d8a8918f255f7df2159e324e75d868ec
Nathan <7nater.nelson@gmail.com>
2020-01-10 19:24:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,3 +32,9 @@ function prependKitten(name) {
   var newKittyList = newKitty.concat(kittens);
   return newKittyList;
 }
+
+function removeLastKitten() {
+  var smallList = kittens.slice(0, kittens.length - 1);
+  return smallList;
+  
+}
